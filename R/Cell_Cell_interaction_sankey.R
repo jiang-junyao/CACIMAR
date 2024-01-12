d83bb824cac9fd2e09a9cc6b4b0a72fde9c0e7f3
lijinlian <13078434436@163.om>
2024-01-11 20:59:52 -0500

    add R file

---
+++ R/Cell_Cell_interaction_sankey.R
@@ -0,0 +1,247 @@
+#' Create sankey plot
+#' @description Build a sankey plot to show the cell-cell interaction profile of two species
+#' @param links dataframe, a links file containing source, target, and weight
+#' @param specie_name1 character, name for specie 1
+#' @param specie_name2 character, name for specie 2
+#' @param output_file character, save sankey plot with this file name
+#' @param colors_file character, file path for the color file, it is NULL by default and it will assign colors automatically
+#' @param brewer.pal_set if colors_file is null, the brewer.pal can be choose, like "Set1", "Set3", "Paired", ......
+#' @param node_width numeric, width of the node
+#' @param node_padding numeric, distance between the node
+#' @param node_stroke_width numeric, width of the stroke around nodes
+#' @param node_corner_radius numeric, the radius of the node
+#' @param drag_x logical, TRUE indicates that the plot can be horizontally dragged
+#' @param drag_y logical, TRUE indicates that the plot can be vertically dragged
+#' @param units character, name for units
+#' @param node_pos_x character, variable used for grouping nodes on the x-axis
+#' @param align character, alignment of the nodes. One of 'right', 'left', 'justify', 'center', 'none'. If 'none', then the labels of the nodes are always to the right of the node
+#' @param scale_node_breadths_by_string logical, Put nodes at positions relatively to string lengths - only work well currently with align='none'
+#' @param show_node_values logical, Show values above nodes. Might require and increased node margin.
+#' @param height numeric, height of the sankey plot for saving
+#' @param width numeric, width of the sankey plot for saving
+#' @param linkColor numeric, color of links
+#' @param link_type character, one of 'bezier', 'l-bezier', 'trapezoid', 'path1' and 'path2'
+#' @param curvature numeric, curvature parameter for bezier links - between 0 and 1
+#' @param link_opacity numeric, opacity of links
+#' @param link_gradient logical, add a gradient to the links
+#' @param node_shadow logical, add a shadow to the nodes
+#' @param node_label_margin numeric, distance between the node and font
+#' @param zoom logical, value to enable (TRUE) or disable (FALSE) zooming
+#' @param iterations numeric, number of iterations in the diagramm layout for computation of the depth (y-position) of each node. Note: this runs in the browser on the client so don't push it too high
+#' @param x_scaling_factor numeric, scale the computed x position of the nodes by this value
+#' @param font_size  numeric, size of the font of the plot
+#' @param ... param of sankeyD3::sankeyNetwork()
+#'
+#' @return a sankey plot
+#' @export
+#'
+#' @examples
+#' # show the weight result
+#' head(all_weight_df_long)
+#' source       target    weight species scale_weight             Source            Source2
+#' 1     Activated MG Activated MG 152.07967      Mm  0.008256281     MmActivated MG     MmActivated MG
+#' 2       Astrocytes Activated MG 230.97254      Mm  0.012539310       MmAstrocytes       MmAstrocytes
+#' 3            Cones Activated MG  51.89929      Mm  0.002817570            MmCones            MmCones
+#' 4     GABAergic AC Activated MG  99.92955      Mm  0.005425093     MmGABAergic AC     MmGABAergic AC
+#' 5   Glycinergic AC Activated MG  78.95619      Mm  0.004286467   MmGlycinergic AC   MmGlycinergic AC
+#' 6 Horizontal cells Activated MG 106.23687      Mm  0.005767513 MmHorizontal cells MmHorizontal cells
+#'
+#' create_sankey(links = all_weight_df_long[, c("Source2", "target", "scale_weight")],
+#' output_file = "sankey_scale_weight.html",
+#' specie_name1 = "Mm",
+#' specie_name2 = "Zf",
+#' colors_file = NULL)
+#'
+create_sankey <- function(links,
+                          specie_name1 = "Mm",
+                          specie_name2 = "Zf",
+                          output_file = "sankey.html",
+                          colors_file = NULL,
+                          brewer.pal_set = "Set3",
+                          node_width = 18,
+                          node_padding = 15,
+                          node_stroke_width = 0,
+                          node_corner_radius = 0,
+                          drag_x = TRUE,
+                          drag_y = TRUE,
+                          units = "TWh",
+                          node_pos_x = "group",
+                          align = "none",
+                          scale_node_breadths_by_string = F,
+                          show_node_values = FALSE,
+                          height = 1400,
+                          width = 1800,
+                          linkColor = "#A0A0A0",
+                          link_type = "bezier",
+                          curvature = 0.5,
+                          link_opacity = 5,
+                          link_gradient = FALSE,
+                          node_shadow = FALSE,
+                          node_label_margin = 5,
+                          zoom = TRUE,
+                          font_family = NULL,
+                          iterations = 0,
+                          x_scaling_factor = 1.4,
+                          font_size = 16,
+                          ...
+){
+  # links
+  colnames(links) <- c("Source", "target", "weight")
+  # nodes
+  library(tidyverse)
+  nodes <- data.frame(name=c(as.character(links$Source), as.character(links$target)) %>% unique())
+  # groups
+  nodes$group <- c(
+    rep(0, sum(grepl(specie_name1, nodes$name))),
+    rep(1, (length(nodes$name) - sum(sum(grepl(specie_name1, nodes$name)), sum(grepl(specie_name2, nodes$name))))),
+    rep(2, sum(grepl(specie_name2, nodes$name)))
+  )
+
+  # node name
+  nodes$name_node <- sub(paste("^", specie_name1, sep = ""), "", nodes$name)
+  nodes$name_node <- sub(paste("^", specie_name2, sep = ""), "", nodes$name_node)
+
+  # node colors
+  if (is.null(colors_file)) {
+    colors <- colorRampPalette(RColorBrewer::brewer.pal(11, brewer.pal_set))(length(unique(nodes$name_node)))
+    colors <- as.data.frame(colors)
+    colnames(colors) <- "colors"
+    colors$celltype <- unique(nodes$name_node)
+    nodes$colors <- colors$colors[match(nodes$name_node, colors$celltype)]
+  } else {
+    colors <- read.table(colors_file)
+    nodes$colors <- colors$colors[match(nodes$name_node, colors$celltype)]
+  }
+
+  # ID start from 0
+  links$IDsource <- match(links$Source, nodes$name) - 1
+  links$IDtarget <- match(links$target, nodes$name) - 1
+  colnames(links) <- c("source", "target", "weight", "IDsource", "IDtarget")
+
+  #Plot
+  p <- sankeyD3::sankeyNetwork(Links = links,
+                               Nodes = nodes,
+                               Source = "IDsource",
+                               Target = "IDtarget",
+                               Value = "weight",
+                               NodeID = "name_node",
+                               nodeWidth = node_width,
+                               nodePadding = node_padding,
+                               nodeStrokeWidth = node_stroke_width,
+                               nodeCornerRadius = node_corner_radius,
+                               NodeColor = "colors",
+                               dragX = drag_x,
+                               dragY = drag_y,
+                               units = units,
+                               NodePosX = node_pos_x,
+                               align = align,
+                               scaleNodeBreadthsByString = scale_node_breadths_by_string,
+                               showNodeValues = show_node_values,
+                               height = height,
+                               width = width,
+                               linkColor = linkColor,
+                               linkType = link_type,
+                               curvature = curvature,
+                               linkOpacity = link_opacity,
+                               linkGradient = link_gradient,
+                               nodeShadow = node_shadow,
+                               nodeLabelMargin = node_label_margin,
+                               zoom = zoom,
+                               iterations = iterations,
+                               xScalingFactor = x_scaling_factor,
+                               fontSize = font_size)
+  # save
+  sankeyD3::saveNetwork(p, output_file)
+  print(p)
+  webshot::webshot(output_file, paste(output_file, ".pdf", sep = ""))
+  return(p)
+}
+
+#' Cell-cell interaction analysis with SingleCellSignalR algorithm
+#' @description Perform cell-cell interaction with SingleCellSignalR algorithm contained in liana package.
+#' @param seurat_obj seurat object, a seurat object with cell types in active idents
+#' @param expre_cell numeric, filter genes expressed in more than expre_cell cells
+#' @param select_DB character, databased used for analysis, more option can run liana::show_resources()
+#' @param target_organism ncbi_taxid' or 'name' of the target organism. See ‘show_homologene' for available organisms via OmnipathR’s 'HomoloGene'
+#' @param method character, method use for cell-cell interaction analysis. By default, we set it to SingleCellSignalR
+#'
+#' @return data frame of cell cell interaction analysis result
+#' @export
+#'
+#' @examples
+#'# Mouse analysis
+#'SingleCellSignalR_mouse_result <- perform_CCI_analysis(seurat_obj=Mm_seurat, target_organism=10090)
+#'
+# Zebrafish analysis
+#'SingleCellSignalR_zebrafish_result <- perform_CCI_analysis(seurat_obj=Zf_seurat, target_organism=7955)
+perform_CCI_analysis <- function(seurat_obj, expre_cell = 10, select_DB = "Consensus", target_organism, method = 'sca') {
+  seurat_obj <- seurat_obj[apply(GetAssayData(seurat_obj, slot = "counts"), 1, function(x) sum(x > 0) > expre_cell),]
+  op_resource <- liana::select_resource(select_DB)[[1]]
+  ortholog_resource <- liana::generate_homologs(op_resource = op_resource, target_organism = target_organism)
+  liana_result <- liana::liana_wrap(seurat_obj,
+                                    method = method,
+                                    resource = 'custom',
+                                    external_resource = ortholog_resource)
+  return(liana_result)
+}
+
+#'  Sum weight of cell-cell interactions
+#' @description Here calculate overall weight of cell-cell interactions within each pair of cell types
+#' @param species1_cci data frame,  cell-cell interactions result with perform_CCI_analysis of species 1
+#' @param species2_cci data frame,  cell-cell interactions result with perform_CCI_analysis of species 2
+#' @param specie_name1 character, name for species 1, like "Mm"
+#' @param specie_name2 character, name for species 2, like "Zf"
+#'
+#' @return data frame of weight result
+#' @export
+#'
+#' @examples
+#' all_weight_df_long <- calculate_Weights(species1_cci = SingleCellSignalR_mouse_result,
+#' species2_cci = SingleCellSignalR_zebrafish_result)
+#'
+#' head(all_weight_df_long)
+#'             source       target    weight species scale_weight             Source            Source2
+#' 1     Activated MG Activated MG 152.07967      Mm  0.008256281     MmActivated MG     MmActivated MG
+#' 2       Astrocytes Activated MG 230.97254      Mm  0.012539310       MmAstrocytes       MmAstrocytes
+#' 3            Cones Activated MG  51.89929      Mm  0.002817570            MmCones            MmCones
+#' 4     GABAergic AC Activated MG  99.92955      Mm  0.005425093     MmGABAergic AC     MmGABAergic AC
+#' 5   Glycinergic AC Activated MG  78.95619      Mm  0.004286467   MmGlycinergic AC   MmGlycinergic AC
+#' 6 Horizontal cells Activated MG 106.23687      Mm  0.005767513 MmHorizontal cells MmHorizontal cells
+calculate_Weights <- function(
+    species1_cci,
+    species2_cci,
+    specie_name1 ="Mm",
+    specie_name2 = "Zf"){
+  species1_all_weight_df_long <- calculateWeights(data = species1_cci,
+                                                  specie_name = specie_name1)
+  species2_all_weight_df_long <- calculateWeights(data = species2_cci,
+                                                  specie_name = specie_name2)
+  # bind df
+  all_weight_df_long <- rbind(species1_all_weight_df_long, species2_all_weight_df_long)
+  all_weight_df_long <- subset(all_weight_df_long, weight > 0)
+  all_weight_df_long$Source <- paste0(all_weight_df_long$species, all_weight_df_long$source)
+  # order for sankey plot
+  all_weight_df_long$target <- as.character(all_weight_df_long$target)
+  all_weight_df_long$Source2 <- all_weight_df_long$Source
+  edit_target <- all_weight_df_long$Source[which(all_weight_df_long$species == specie_name2)]
+  edit_source <- all_weight_df_long$target[which(all_weight_df_long$species == specie_name2)]
+  all_weight_df_long$Source2[which(all_weight_df_long$species == specie_name2)] <- edit_source
+  all_weight_df_long$target[which(all_weight_df_long$species == specie_name2)] <- edit_target
+  return(all_weight_df_long)
+}
+
+calculateWeights <- function(data, specie_name) {
+  ccc_weight <- reshape2::dcast(data[, c(2, 3, 11)],
+                                source ~ target,
+                                sum,
+                                value.var = 'LRscore')
+  rownames(ccc_weight) <- ccc_weight[, 1]
+  all_weight_df_long <- reshape2::melt(ccc_weight,
+                                       id.vars = "source",
+                                       variable.name = "target",
+                                       value.name = "weight")
+  all_weight_df_long$species <- specie_name
+  all_weight_df_long$scale_weight <- all_weight_df_long$weight/sum(all_weight_df_long$weight)
+
+  return(all_weight_df_long)
+}
---
+++ R/ChordDiagram.R
@@ -0,0 +1,193 @@
+#' Make a ChordDiagram
+#' @description Construct a ChordDiagram to show the conservation score for intercellular interactions
+#' @param net data frame, a data frame contain the source, target, and weight
+#' @param Score_factor_size numeric, scale the weight score for showing
+#' @param brewer_pal_used character, the brewer_pal in RColorBrewer, like "Set1",  "Set3", "Paired"
+#' @param grid.col vector, color vectors with names. By default, it is NULL and it will asign colors automatically.
+#' @param grid.border character, corlors for borders of grids. If it is NULL, the border color is same as grid color
+#' @param is_link_colors_threshold logical, whether change the colors of links which below a threshold to a  specific color
+#' @param link_colors_threshold numeric, the threshold used to change colors for links
+#' @param link_colors_down_threshold_col character, specific corlor for links which below a threshold
+#' @param filename character, save the plot with this filename
+#' @param order_grid vector, a ordered vector for grid. By default, it is NULL
+#' @param directional  Directions for the links. 1 means the direction is from the first column in df to the second column, -1 is the reverse, 0 is no direction, and 2 for two directional. The value can be a vector which has same length as number of rows in df
+#' @param direction.type type for representing directions. Can be one or two values in "diffHeight" and "arrows"
+#' @param diffHeight numeric, The difference of height between two 'roots' if directional is set to TRUE
+#' @param annotationTrack annotation for the track. It can set to c("name","grid", "axis"), or one of them.
+#' @param reduce numeric, if the ratio of the width of certain grid compared to the whole circle is less than this value, the grid is removed on the plot. Set it to value less than zero if you want to keep all tiny grid.
+#' @param link.arr.type type for the arrow, "big.arrow" is set by default
+#' @param link.border character, corlors for links
+#' @param link.lwd numeric, width for link borders
+#' @param link.sort logical, whether sort links on every sector based on the width of the links on it
+#' @param link.decreasing logical, for link.sort
+#' @param link.largest.ontop  logical, controls the order of adding links
+#' @param link.overlap logical, whether the links that come or end in a same sector overlap
+#' @param transparency numeric, transparency for link colors
+#' @param cex numeric, font size for the text in annotationTrack
+#' @param ylim_edit numeric, control the text height from the grid
+#' @param facing control the direction of the text in annotationTrack, it can be one of c("inside", "outside", "reverse.clockwise", "clockwise", "downward", "bending", "bending.inside", "bending.outside")
+#' @param niceFacing logical, adjusted the text to fit human eyes
+#' @param col character, colors for the text in annotationTrack
+#' @param adj offset for text, like c(0, 0.5). By default the text position adjustment is either horizontal or vertical in the canvas coordinate system.
+#' @param start.degree numeric, rotation the ChordDiagram with this angle
+#' @param picture_width numeric, width of the ChordDiagram for saving
+#' @param picture_height numeric, height of the ChordDiagram for saving
+#'
+#' @return saving ChordDiagram in current directory
+#' @export
+#'
+#' @examples
+#' load(system.file("extdata", "CCC_conserved_sumary.rda", package = "CACIMAR"))
+#' cci_data = CCC_conserved_sumary[, c("Source", "Target", "score_weight")]
+#' ChordDiagram(net = cci_data, filename = "chordDiagram_cutoff0.75.pdf", link_colors_threshold = 0.75)
+#'
+ChordDiagram <- function(net,
+                         Score_factor_size = 20,
+                         brewer_pal_used = "Set3",
+                         grid.col = NULL,
+                         grid.border = "#F8F8F8",
+                         is_link_colors_threshold = TRUE,
+                         link_colors_threshold = 0.5,
+                         link_colors_down_threshold_col = "grey",
+                         filename = "chordDiagram.pdf",
+                         order_grid = NULL,
+                         directional = 1,
+                         direction.type = c("arrows"),
+                         diffHeight = -0.03,
+                         annotationTrack = "grid",
+                         reduce = -1,
+                         link.arr.type = "big.arrow",
+                         link.border = "#F8F8F8",
+                         link.lwd = 0.6,
+                         link.sort = TRUE,
+                         link.decreasing = TRUE,
+                         link.largest.ontop = TRUE,
+                         link.overlap = F,
+                         transparency = 0,
+                         cex = 1.3,
+                         ylim_edit = 0,
+                         facing = "clockwise",
+                         niceFacing = TRUE,
+                         col = "black",
+                         adj = c(0, 0.5),
+                         start.degree = 70,
+                         picture_width = 7,
+                         picture_height = 9
+) {
+  # link value
+  colnames(net) <- c("source", "target", "Score")
+  net$Score <- 2^(net$Score*Score_factor_size)
+
+  if (is.null(grid.col)) {
+    grid.col <- colorRampPalette(RColorBrewer::brewer.pal(11, brewer_pal_used))(length(c(unique(net$source), unique(net$target))))
+    grid.col <- setNames(grid.col, c(unique(net$source), unique(net$target)))
+  } else {
+    grid.col <- grid.col
+  }
+
+  # grid colors
+  grid.cols_df <- as.data.frame(grid.col)
+  colnames(grid.cols_df) <- c("colors")
+  grid.cols_df$celltype <- names(grid.col)
+
+  # link colors
+  net$colors <- grid.cols_df$colors[match(net$source, grid.cols_df$celltype)]
+  net$link_colors <- net$colors
+
+  if (is_link_colors_threshold) {
+    Score <- as.vector(unlist(net$Score))
+    median_value <- quantile(Score, probs = link_colors_threshold)
+    net$link_colors[which(net$Score < median_value)] <- link_colors_down_threshold_col
+  }
+
+  pdf(file = filename, width = picture_width, height = picture_height)
+  circlize::circos.par(start.degree = start.degree)
+  circlize::chordDiagram(net,
+                         grid.col = grid.col,
+                         grid.border = grid.border,
+                         col = net$link_colors,
+                         order = order_grid,
+                         directional = directional,
+                         direction.type = direction.type,  # "diffHeight"
+                         diffHeight = diffHeight,
+                         annotationTrack = annotationTrack,
+                         reduce = reduce,
+                         link.arr.type = link.arr.type,
+                         link.border = link.border,
+                         link.lwd = link.lwd,
+                         link.sort = link.sort,
+                         link.decreasing = link.decreasing,
+                         link.largest.ontop = link.largest.ontop,
+                         link.overlap = link.overlap,
+                         transparency = transparency,
+                         preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(net)))))
+  )
+  # text
+  for(si in circlize::get.all.sector.index()) {
+    xlim = circlize::get.cell.meta.data("xcenter", sector.index = si, track.index = 1)
+    ylim = circlize::get.cell.meta.data("ylim", sector.index = si, track.index = 1)
+    circlize::circos.text(xlim,
+                          ylim+ylim_edit,
+                          si,
+                          sector.index = si,
+                          track.index = 1,
+                          cex = cex,
+                          facing = facing,
+                          niceFacing = niceFacing,
+                          col = col,
+                          adj = adj)
+  }
+  dev.off()
+}
+
+#' Plot ligand-receptor pairs in a dot plot
+#' @description Construct a dot plot to show the conserved ligand-receptor pairs
+#' @param LR_data data frame only containing 3 column representing the ligand-receptor pairs, cell type pairs, and groups
+#' @param fill_colors vector, colors for the points
+#' @param brewer.pal_set if fill_colors = NULL, this can set the brewer.pal for colors used for the text and points. By default, brewer.pal_set = "Paired"
+#' @param color_x By default, colors for x text is set to be "black"
+#' @param color_x_in_group if color_x_in_group = T, points colors will be used for x text
+#' @param color_y By default, colors for y text is set to be "black", or it can be set to any colors you want, like "red", "blue"
+#' @param size numeric, size for points
+#' @param shape numeric, shape for points
+#' @param colour colors for points
+#' @param stroke numeric, width of the point’s outline
+#' @param theme_set theme used. By default it is theme_bw()
+#' @param text_size numeric, size of the text in this plot
+#' @param text_colour colors for the text in this plot
+#' @param legend.position the position of the legend. By default, we don't show the legend
+#'
+#' @return A dot plot. Row is the ligand-receptor pairs, while column is the cell type pairs
+#' @export
+#'
+#' @examples
+#' show_LR_pairs(LR_data = LRpair_show_0.75,  color_x_in_group = T)
+#'
+show_LR_pairs <- function(LR_data, fill_colors = NULL, brewer.pal_set = "Paired", color_x = "black", color_x_in_group = F, color_y = "black", size = 9, shape = 21, colour = "white", stroke = 2, theme_set = theme_bw(), text_size = 40, text_colour = "black", legend.position = "none") {
+  stopifnot(nrow(LR_data) == 3)
+  colnames(LR_data) <- c("LR", "Cell_Pair", "group")
+  if (is.null(fill_colors)) {
+    color_fill <- colorRampPalette(RColorBrewer::brewer.pal(11, brewer.pal_set))(length(unique(LR_data$group)))
+    color_fill <- setNames(color_fill, unique(LR_data$group))
+  } else {
+    color_fill <- fill_colors
+  }
+
+  if (color_x_in_group) {
+    x_text_colors <- color_fill[as.vector(unlist(sapply(unique(LR_data$Cell_Pair), function(x) strsplit(x, "_")[[1]][1])))]
+    color_x <- x_text_colors
+  } else {
+    color_x <- color_x
+  }
+  print(color_x)
+  p <- ggplot2::ggplot(LR_data) +
+    ggplot2::geom_point(mapping = aes(x = Cell_Pair, y = LR, fill = group), size = size, shape = shape, colour = colour, stroke = stroke) +
+    theme_set +
+    theme(text = element_text(size = text_size, colour = text_colour),
+          axis.text.x = element_text(colour = color_x, angle = 90, hjust = 1, vjust = 0.5),
+          axis.text.y = element_text(colour = color_y),
+          legend.position = legend.position) +
+    labs(y = "", x = "") +
+    scale_fill_manual(values = color_fill)
+  return(p)
+}
